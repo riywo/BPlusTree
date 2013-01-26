45877fad1e145cf12b6af0db25ac1b54b29678a0
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-25 22:37:11 -0800

    insert node

--- lib/b_plus_tree/linked_list.rb
+++ lib/b_plus_tree/linked_list.rb
@@ -1,9 +1,37 @@
 class BPlusTree::LinkedList
-  def initialize
+  attr_reader :first
+  attr_reader :last
 
+  def initialize
+    @first = Node.new(nil, nil)
+    @last  = @first
   end
 
   def insert(key, value)
+    node = Node.new(key, value)
+
+    if @first.key.nil?
+      @first = node
+      @last  = node
+    else
+      @last.next(node)
+      @last = node
+    end
     true
+  end
+
+  class Node
+    attr_reader   :key
+    attr_accessor :value
+
+    def initialize(key, value)
+      @next_node = nil
+      @key       = key
+      @value     = value
+    end
+
+    def next(node)
+      @next_node = node
+    end
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,5 +5,28 @@ describe BPlusTree::LinkedList do
 
   it 'should insert one node' do
     @list.insert(1, 'one').should == true
+    @list.first.key.should   == 1
+    @list.first.value.should == 'one'
+    @list.last.key.should    == 1
+    @list.last.value.should  == 'one'
+  end
+
+  it 'should insert two nodes' do
+    @list.insert(1, 'one').should == true
+    @list.insert(2, 'two').should == true
+    @list.first.key.should   == 1
+    @list.first.value.should == 'one'
+    @list.last.key.should    == 2
+    @list.last.value.should  == 'two'
+  end
+
+  it 'should insert three nodes' do
+    @list.insert(1, 'one').should == true
+    @list.insert(2, 'two').should == true
+    @list.insert(3, 'three').should == true
+    @list.first.key.should   == 1
+    @list.first.value.should == 'one'
+    @list.last.key.should    == 3
+    @list.last.value.should  == 'three'
   end
 end
