110efba1223a63d46c46e0936119dce3d5caa237
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-26 08:16:59 -0800

    add insert test

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -9,4 +9,21 @@ describe BPlusTree::Page do
     @page.supremum.key.should == 1
     @page.keys.should == [1]
   end
+
+  it 'should insert two record' do
+    @page.insert(1, 'one').should == 1
+    @page.insert(2, 'two').should == 2
+    @page.infimum.key.should  == 1
+    @page.supremum.key.should == 2
+    @page.keys.should == [1, 2]
+  end
+
+  it 'should insert three record' do
+    @page.insert(1, 'one').should   == 1
+    @page.insert(2, 'two').should   == 2
+    @page.insert(3, 'three').should == 3
+    @page.infimum.key.should  == 1
+    @page.supremum.key.should == 3
+    @page.keys.should == [1, 2, 3]
+  end
 end
